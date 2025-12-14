10d3840c252d7be8bdf4b482f994a248bf50de09
Elian Meaca <126032319+eth3rino@users.noreply.github.com>
2025-12-14 13:57:03 -0300

    small typo on password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ export async function login(req, res) {
     console.log(req.body)
     const { email, password } = req.body;
 
-    if (email === "test@gmail.com" && password === "123") {
+    if (email === "test@gmail.com" && password === "123456") {
         const user = {email: email, id: "123"};
         const token = await gen_token(user);
         res.json({token});
